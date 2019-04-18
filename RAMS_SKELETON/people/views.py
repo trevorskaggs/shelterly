a42381fccaa4bdea06670044575da7fc711b7d38
spencercarroll <scarroll10@csuchico.edu>
2019-04-18 00:20:36 -0700

    split address into address and apt fields made state field into dropdown

--- RAMS_SKELETON/people/views.py
+++ RAMS_SKELETON/people/views.py
@@ -11,7 +11,7 @@ class OwnerListView(generic.ListView):
 class OwnerNewView(generic.edit.CreateView):
     model = Owner
     template_name = 'owner_new.html'
-    fields = ['first_name', 'last_name', 'home_phone', 'work_phone', 'cell_phone', 'best_contact', 'drivers_license', 'address', 'city', 'state', 'zip_code']
+    fields = ['first_name', 'last_name', 'home_phone', 'work_phone', 'cell_phone', 'best_contact', 'drivers_license', 'address', 'apartment', 'city', 'state', 'zip_code']
 
 class OwnerDetailView(generic.DetailView):
     model = Owner
