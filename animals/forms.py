653e16f0fb60ffd7ceba56dd809de2ba74f13d6f
Trevor Skaggs <skaggs.trevor@gmail.com>
2019-05-11 16:21:54 +0000

    reorg animal form

--- animals/forms.py
+++ animals/forms.py
@@ -4,57 +4,40 @@ from animals.models import Animal
 class AnimalForm(forms.ModelForm):
 
     def save(self, owner=None):
-        animal = super(AnimalForm, self).save()
-        import ipdb; ipdb.set_trace()
         if owner:
             animal.owner = owner
             animal.save()
 
     def set_initial_location(self, location_object):
         for field_name, field_value in location_object.get_location_fields():
-            self.fields[field_name].initial = field_value
+            try:
+                self.fields[field_name].initial = field_value
+            except:
+                pass
 
+    def set_species_properties(self, species):
+        self.fields['species'].initial = species
+        self.fields['pcolor'].label = '%s Primary Color' % species.capitalize()
+        self.fields['scolor'].label = '%s Secondary Color' % species.capitalize()
+        self.fields['markings'].label = '%s Markings' % species.capitalize()
+
+    class Meta:
+        model = Animal
+        exclude = ('latitude', 'longitude', 'request', 'owner')
 
 class DogForm(AnimalForm):
 
     def __init__(self, *args, **kwargs):
         super(DogForm, self).__init__(*args, **kwargs)
-        self.initial['species'] = 'dog'
+        self.set_species_properties('dog')
 
-    class Meta:
-        model = Animal
-        labels = {
-            "pcolor" : "Dog Primary Color",
-            "scolor" : "Dog Secondary Color",
-            "markings" : "Dog Markings",
-        }
-
-        exclude = ('latitude', 'longitude', 'request', 'owner')
 
 class CatForm(AnimalForm):
 
     def __init__(self, *args, **kwargs):
         super(CatForm, self).__init__(*args, **kwargs)
-        self.initial['species'] = 'cat'
-
-    class Meta:
-        model = Animal
-        labels = {
-            "pcolor" : "Cat Primary Color",
-            "scolor" : "Cat Secondary Color",
-            "markings" : "Cat Markings",
-        }
-
-        exclude = ('latitude', 'longitude', 'request', 'owner')
+        self.set_species_properties('cat')
 
 class OtherForm(AnimalForm):
 
-    class Meta:
-        model = Animal
-        labels = {
-            "pcolor" : "Primary Color",
-            "scolor" : "Secondary Color",
-            "markings" : "Markings",
-        }
-
-        exclude = ('latitude', 'longitude', 'request', 'owner')
+    pass
