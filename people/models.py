d9853dd61cd51fe602fe5551ec3f6b560381ada1
Trevor Skaggs <skaggs.trevor@gmail.com>
2019-05-11 16:44:38 +0000

    Add evac app

--- people/models.py
+++ people/models.py
@@ -31,8 +31,11 @@ class Reporter(Person):
         return self.field_name
 
 class Worker(Person):
-    class Meta:
-        ordering = []
+
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    cell_phone = models.CharField(max_length=50, blank=False, null=False)
+    agency_id = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return self.field_name
+        return '{} {}'.format(self.first_name, self.last_name)
