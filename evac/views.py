3cc10c3a7dcea72b3ded7dbab2e87b4405f4b251
Trevor Skaggs <skaggs.trevor@gmail.com>
2019-08-03 17:26:47 -0700

    Simplify views/forms

--- evac/views.py
+++ evac/views.py
@@ -9,17 +9,16 @@ def evac_landing(request):
 
 def evac_team(request, pk=None):
     evac_team = EvacTeam.objects.get(pk=pk) if pk else None
-    if request.POST:
-        form = EvacTeamForm(request.POST, instance=evac_team)
+    form = EvacTeamForm(request.POST or None, instance=evac_team)
+    if request.POST and form.is_valid():
+        form.save()
         return redirect('evac:evac_landing')
-    form = EvacTeamForm(instance=evac_team)
     return render(request, 'evac_team.html', {'form':form})
 
 def team_member(request, pk=None):
     team_member = TeamMember.objects.get(pk=pk) if pk else None
-    if request.POST:
-        form = TeamMemberForm(request.POST, instance=team_member)
+    form = TeamMemberForm(request.POST or None, instance=team_member)
+    if request.POST and form.is_valid():
         form.save()
         return redirect('evac:evac_landing')
-    form = TeamMemberForm(instance=team_member)
     return render(request, 'team_member.html', {'form':form})
