617670e85306470ea7ea44943b4c1be672caf552
Trevor Skaggs <skaggs.trevor@gmail.com>
2019-08-03 18:38:32 -0700

    Add VT

--- location/models.py
+++ location/models.py
@@ -7,7 +7,7 @@ STATE_CHOICES = (
     ('MI', "MI"),('MN', "MN"),('MS', "MS"),('MO', "MO"),('MT', "MT"),('NE', "NE"),('NV', "NV"),
     ('NH', "NH"),('NJ', "NJ"),('NM', "NM"),('NY', "NY"),('NC', "NC"),('ND', "ND"),('OH', "OH"),
     ('OK', "OK"),('PA', "PA"),('RI', "RI"),('SC', "SC"),('SD', "SD"),('TN', "TN"),('TX', "TX"),
-    ('VA', "VA"),('WA', "WA"),('WV', "WV"),('WI', "WI"),('WY', "WY"),
+    ('VA', "VA"),("VT", "VT"),('WA', "WA"),('WV', "WV"),('WI', "WI"),('WY', "WY"),
 )
 
 # Create your models here.
