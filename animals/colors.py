1a8dbb0474bbf6b6dd1cb2611661a5d06bb9eed5
spencercarroll <scarroll10@csuchico.edu>
2019-10-29 10:22:28 -0400

    added cat color/pattern

--- animals/colors.py
+++ animals/colors.py
@@ -23,3 +23,30 @@ DOG_PATTERN_CHOICES = (
     ('sable', 'Sable'),
     ('hairless', 'Hairless'),
 )
+
+## Source: https://en.wikipedia.org/wiki/Cat_coat_genetics
+CAT_COLOR_CHOICES = (
+    ('black', 'Black'),
+    ('grey', 'Grey'),
+    ('chocolate', 'Chocolate'),
+    ('cinnamon', 'Cinnamon'),
+    ('caramel', 'Caramel')
+    ('white', 'White'),
+    ('black white', 'Black / White'),
+    ('grey white', 'Grey / White'),
+    ('brown white', 'Brown / White'),
+    ('oranage white', 'Orange / White'),
+    ('black brown', 'Black / Brown'),
+)
+
+## Source: https://en.wikipedia.org/wiki/Cat_coat_genetics
+CAT_PATTERN_CHOICES = (
+    ('solid', 'Solid'),
+    ('bicolor', 'Bicolor'),
+    ('bengal', 'Bengal')
+    ('colorpoint', 'Colorpoint'),
+    ('calico', 'Calico'),
+    ('tortoiseshell', 'Tortoiseshell'),
+    ('tabby', 'Tabby'),
+    ('hairless', 'Hairless'),
+)
