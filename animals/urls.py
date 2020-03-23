b1aabaca64d28b00c50861719987948e444d9a15
Robbie Meyers <robbie.meyers@gmail.com>
2020-03-23 18:49:48 -0400

    drfify

--- animals/urls.py
+++ animals/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
 app_name = 'animals'
+router = DefaultRouter()
+router.register(r'animals', views.AnimalViewSet)
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('<int:pk>/', views.animal_detail, name='animal_detail'),
     path('<int:pk>/delete', views.animal_delete, name='animal_delete'),
     path('<int:pk>/edit', views.animal_edit, name="animal_edit"),
