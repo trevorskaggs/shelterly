7562443eac82c3b6cbe886351e68a4410c31ce3b
Robbie Meyers <robbie.meyers@gmail.com>
2020-04-07 19:26:22 -0400

    use axios for fetching data

--- frontend/src/animals/AnimalTables.js
+++ frontend/src/animals/AnimalTables.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import Table from ".././components/Table";
 
-const ANIMALS_URL = 'http://127.0.0.1:8000/animals/list';
+const ANIMALS_URL = 'http://127.0.0.1:8000/animals/api/animals';
 function AnimalTable() {
     const columns = React.useMemo(
         () => [
@@ -10,30 +10,39 @@ function AnimalTable() {
                 Header: 'Name',
                 accessor: 'name'
             }
-        ]
+        ],
+        []
     )
 
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({animals: [], isFetching: false});
 
     // why async? i don't know!
-    async function getData() {
-      const response = await fetch(
-        'ANIMALS_URL',
-      );
-      const data = await response.json();
-      // Previously initialized setter for data const.
-      setData(data.results);
-    }
-  
-    // Hook interface, use this instead of extending from Component
-    // and using componentDidMount, etc.
     useEffect(() => {
-      getData();
-    }, []);
+        let source = axios.CancelToken.source();
+        const fetchAnimals = async () => {
+          setData({animals: data.animals, isFetching: true});
+          // Fetch EvacTeam data.
+          await axios.get(ANIMALS_URL, {
+            cancelToken: source.token,
+          })
+          .then(response => {
+            setData({animals: response.data, isFetching: false});
+          })
+          .catch(e => {
+            console.log(e);
+            setData({animals: data.animals, isFetching: false});
+          });
+        };
+        fetchAnimals();
+        // Cleanup.
+        return () => {
+          source.cancel();
+        };
+      }, []);
 
     return (
         <div>
-          <Table columns={columns} data={data} />
+          <Table columns={columns} data={data.animals} />
         </div>
     );
 }
