98b2a7e033b1e1f7b6ce1c9b61848e8d87bc39b8
Robbie Meyers <robbie.meyers@gmail.com>
2020-04-27 16:24:01 -0400

    remove stale trash, fixup generic form components

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useField } from 'formik';
-import { Label, Input } from 'reactstrap';
+import { useFormikContext, useField } from 'formik';
+import { FormFeedback, Label, Input } from 'reactstrap';
+import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // ...props is shorthand for "rest of the items in this array". So the 1st item is
@@ -11,10 +12,10 @@ const TextInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
     <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
+      <Label htmlFor={props.id || props.name}>{label}</Label>
+      <Input className={meta.touched && meta.error ? "is-invalid" : null} {...field} {...props} />
       {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
+        <FormFeedback>{meta.error}</FormFeedback>
       ) : null}
     </>
   );
@@ -27,10 +28,10 @@ const Checkbox = ({ children, ...props }) => {
   const [field, meta] = useField({ ...props, type: 'checkbox' });
   return (
     <>
-      <label className="checkbox">
+      <Label className="checkbox">
         <input type="checkbox" {...field} {...props} />
         {children}
-      </label>
+      </Label>
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
       ) : null}
@@ -38,14 +39,24 @@ const Checkbox = ({ children, ...props }) => {
   );
 };
 
-const Select = ({ label, ...props }) => {
+const DropDown = ({ options, label, ...props }) => {
+  const { setFieldValue, setFieldTouched } = useFormikContext();
   const [field, meta] = useField(props);
+
+  function handleOptionChange(selection) {
+    setFieldValue(props.name, selection);
+  }
+
+  function updateBlur() {
+    setFieldTouched(props.name, true);
+  }
+
   return (
     <>
       <Label htmlFor={props.id || props.name}>{label}</Label>
-      <Input type="select" {...field} {...props} />
+      <Select options={options} {...field} {...props} onBlur={updateBlur} onChange={handleOptionChange}/>
       {/* {meta.touched && meta.error ? (
-        <StyledErrorMessage>{meta.error}</StyledErrorMessage>
+        <span>{meta.error}</span>
       ) : null} */}
     </>
   );
@@ -60,5 +71,5 @@ const MultiSelect = ({ label, ...props }) => {
       </>
     );
   };
-  
-export { TextInput, Checkbox, Select, MultiSelect };
+
+export { TextInput, Checkbox, DropDown, MultiSelect };
