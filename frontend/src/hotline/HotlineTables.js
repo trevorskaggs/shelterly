496a800bb863684a4d580526c16cfd31ca333232
Alex Mountain <alexander.g.mountain@gmail.com>
2020-04-29 16:58:00 -0400

    Simple working SR search page.

--- frontend/src/hotline/HotlineTables.js
+++ frontend/src/hotline/HotlineTables.js
@@ -1,33 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-import { useQueryParams } from 'raviger';
-import BootstrapTable from 'react-bootstrap-table-next'
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-
-const columns = [
-  {
-    dataField: 'id',
-    text: 'SR',
-  }, 
-  {
-    dataField: 'owner_name',
-    text: 'Owner'
-  },
-  {
-    dataField: 'reporter_name',
-    text: 'Reporter'
-  },
-]
+import Table from '../components/Table';
+import { Input } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function ServiceRequestTable() {
 
-  const [data, setData] = useState({service_requests: [], isFetching: false});
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'SR',
+        accessor: 'id'
+      },
+      {
+        Header: 'Owner',
+        accessor: 'owner_name'
+      },
+      {
+        Header: 'Reporter',
+        accessor: 'reporter_name'
+      },
+      // {
+      //   Header: 'Team Members',
+      //   accessor: 'evac_team_member_names',
+      //   Cell: ({ cell: { value } }) =>
+      //     <div><a href={"/evac/evacteam/"+value+"/"}>Evac Team {value}</a></div>
+      // }
+    ],
+    []
+  )
 
-  // Identify any query param data.
-  const [queryParams] = useQueryParams();
-  const {
-    search = '',
-  } = queryParams;
+  const [data, setData] = useState({service_requests: [], isFetching: false});
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Update searchTerm when field input changes.
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Use searchTerm to filter service_requests.
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    let source = axios.CancelToken.source();
+    setData({service_requests: [], isFetching: true});
+    // Fetch ServiceRequest data filtered searchTerm.
+    await axios.get('http://localhost:8000/hotline/api/servicerequests/?search=' + searchTerm, {
+      cancelToken: source.token,
+    })
+    .then(response => {
+      setData({service_requests: response.data, isFetching: false});
+    })
+    .catch(error => {
+      console.log(error.response);
+      setData({service_requests: [], isFetching: false});
+    });
+  }
 
   // Hook for initializing data.
   useEffect(() => {
@@ -35,7 +63,7 @@ export function ServiceRequestTable() {
     const fetchServiceRequests = async () => {
       setData({service_requests: [], isFetching: true});
       // Fetch ServiceRequest data.
-      await axios.get('http://localhost:8000/hotline/api/servicerequests/?search=' + search, {
+      await axios.get('http://localhost:8000/hotline/api/servicerequests/', {
         cancelToken: source.token,
       })
       .then(response => {
@@ -51,11 +79,21 @@ export function ServiceRequestTable() {
     return () => {
       source.cancel();
     };
-  }, [search]);
+  }, []);
 
   return (
     <div>
-      <BootstrapTable keyField='id' data={ data.service_requests } columns={columns}/>
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="Search"
+            name="searchTerm"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+        <button className="btn btn-warning btn-lg btn-block mb-2">Search!</button>
+        </form>
+      <Table data={data.service_requests} columns={columns}/>
       <p>{data.isFetching ? 'Fetching teams...' : ''}</p>
     </div>
   )
