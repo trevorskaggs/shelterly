27fb9d493934dd0ea966cbadff6107b49a2d98b1
Robbie Meyers <robbie.meyers@gmail.com>
2020-08-17 03:18:15 +0000

    add more tests, cleanup

--- frontend/src/evac/Evac.test.js
+++ frontend/src/evac/Evac.test.js
@@ -1,41 +1,31 @@
 import React from "react";
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import {cleanup, fireEvent, render, getByLabelText, getByText, findByText, waitFor, waitForElementToBeRemoved} from '@testing-library/react';
-import Evac from "./Evac"
-
-import { EvacTeamForm } from "./EvacForms"
-import { EvacTeamTable } from "./EvacTables"
+import { render, waitForElementToBeRemoved } from "@testing-library/react";
+import Evac from "./Evac";
+import { EvacTeamForm } from "./EvacForms";
+import { EvacTeamTable } from "./EvacTables";
 
 describe("Render evac", () => {
-    it("Render Evac", () => {
-        const { getByText } = render(<Evac />)
-        expect(getByText(/NEW TEAM/)).toBeTruthy()
-        expect(getByText(/TEAM LIST/)).toBeTruthy()
-        expect(getByText(/DEPLOY/)).toBeTruthy()
-        expect(getByText(/DEBRIEF/)).toBeTruthy()
-        expect(getByText(/BACK/)).toBeTruthy()
-    });
+  it("Render Evac", () => {
+    const { getByText } = render(<Evac />);
+    expect(getByText(/NEW TEAM/)).toBeTruthy();
+    expect(getByText(/TEAM LIST/)).toBeTruthy();
+    expect(getByText(/DEPLOY/)).toBeTruthy();
+    expect(getByText(/DEBRIEF/)).toBeTruthy();
+    expect(getByText(/BACK/)).toBeTruthy();
+  });
 
-    it("Render new team form", async () => {
-        const { getByText, findByText} = render(<EvacTeamForm />)
-        expect(getByText(/Evac Team Members*/)).toBeTruthy()
-        expect(getByText(/Callsign*/)).toBeTruthy()
-        expect(getByText(/Save/)).toBeTruthy()
-        expect(getByText(/Cancel/)).toBeTruthy()
+  it("Render new team form", async () => {
+    const { getByText, findByText } = render(<EvacTeamForm />);
+    expect(getByText(/Evac Team Members*/)).toBeTruthy();
+    expect(getByText(/Callsign*/)).toBeTruthy();
+    expect(getByText(/Save/)).toBeTruthy();
+    expect(getByText(/Cancel/)).toBeTruthy();
+  });
 
-    });
-
-    it("Render team list table", async () => {
-        let mockAdapter = new MockAdapter(axios);
-        mockAdapter.onGet('http://localhost:8000/evac/api/evacteam/').reply(200, {
-            'evac_teams':[{'id':1, 'evac_team_member_names': ['x', 'y']}],
-            isFetching:false
-        })
-        const { getByText, findByText} = render(<EvacTeamTable />)
-        expect(getByText(/Evac Team/)).toBeTruthy()
-        expect(getByText(/Team Members/)).toBeTruthy()
-        await waitForElementToBeRemoved(() => getByText("Fetching teams..."));
-
-    });
-})
+  it("Render team list table", async () => {
+    const { getByText, findByText } = render(<EvacTeamTable />);
+    expect(getByText(/Evac Team/)).toBeTruthy();
+    expect(getByText(/Team Members/)).toBeTruthy();
+    await waitForElementToBeRemoved(() => getByText("Fetching teams..."));
+  });
+});
--- frontend/src/hotline/Hotline.test.js
+++ frontend/src/hotline/Hotline.test.js
@@ -1,91 +1,53 @@
 import React from "react";
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-
-import {cleanup, fireEvent, render, getByLabelText, getByText, findByText, waitFor} from '@testing-library/react';
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import {
+  render,
+  screen,
+} from "@testing-library/react";
 import Hotline from "./Hotline";
-import { PersonView, ServiceRequestView } from "./HotlineViews";
+import { ServiceRequestView } from "./HotlineViews";
 import { ServiceRequestTable } from "./HotlineTables";
 
+const server = setupServer(
+  rest.get("/hotline/api/servicerequests/1/", (req, res, ctx) => {
+    return res(
+      ctx.json({ data: { id: 2, reporter: "Jane Doe", address: blah } })
+    );
+  })
+  // rest.get('/hotline/api/servicerequests/', (req, res, ctx) => {
+  //   return res(ctx.json({ data: {service_requests:[{'id': 3}]}}))
+  // }),
+);
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers());
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
+
 describe("Render hotline", () => {
-    it("Render hotline", () => {
-        const { getAllByText, getByText } = render(<Hotline />)
-        expect(getAllByText(/OWNER CALLING/)).toBeTruthy()
-        expect(getByText(/NON-OWNER CALLING/)).toBeTruthy()
-        expect(getByText(/SEARCH SERVICE REQUEST/)).toBeTruthy()
-    });
-})
-describe("Render PersonView", () => {
-    // it("Create Reporter", () => {
-    //     delete global.window.location;
-    //     global.window = Object.create(window);
-    //     Object.defineProperty(window, "location", {
-    //         value: {
-    //            pathname: '/hotline/reporter/new'
-    //         },
-    //         writable: true
-    //     });
-    //     const { getAllByText, getByText } = render(<PersonView />)
-    //     expect(getByText(/Reporter Details/)).toBeTruthy()
-    // });
-
-    // it("Create Owner", () => {
-    //     delete global.window.location;
-    //     global.window = Object.create(window);
-    //     Object.defineProperty(window, "location", {
-    //         value: {
-    //            pathname: '/hotline/owner/new'
-    //         },
-    //         writable: true
-    //     });
-    //     const { getAllByText, getByText } = render(<PersonView />)
-    //     expect(getByText(/Owner Details/)).toBeTruthy()
-    // });
-
-    it("Existing owner fields populated", async () => {
-        let mockAdapter = new MockAdapter(axios);
-        mockAdapter.onGet('http://localhost:3000/people/api/person/1/').reply(200, {
-            first_name:'Leroy',
-            last_name: 'Jenkins',
-            phone: '123456789',
-            email: 'test@test.com',
-            address: '123 Main St.'
-
-        })
-        const { getAllByText, findByText } = render(<PersonView id={1} />)
-        expect(findByText(/Leroy Jenkins/)).toBeTruthy()
-        expect(findByText(/123456789/)).toBeTruthy()
-        expect(findByText(/123456789/)).toBeTruthy()
-        expect(findByText(/123 Main St./)).toBeTruthy()
-    });
-})
+  it("Render hotline", () => {
+    render(<Hotline />);
+    expect(screen.getAllByText(/OWNER CALLING/)).toBeTruthy();
+    expect(screen.getByText(/NON-OWNER CALLING/)).toBeTruthy();
+    expect(screen.getByText(/SEARCH SERVICE REQUEST/)).toBeTruthy();
+  });
+});
 
 describe("Render ServiceRequestView", () => {
-    it("Existing service request fields populated", () => {
-        let mockAdapter = new MockAdapter(axios);
-        mockAdapter.onGet('http://localhost:3000/hotline/api/servicerequests/1/').reply(200, {
-            directions:'123 Main St.'
-        })
-        const {getByText } = render(<ServiceRequestView id={1} />)
-        expect(getByText(/Address/)).toBeTruthy()
-    });
-})
+  it("Service request details loads", async () => {
+    render(<ServiceRequestView id={1} />);
+    expect(screen.getByText(/Service Request/)).toBeTruthy();
+  });
+});
 
 describe("Render ServiceRequestTable", () => {
-    it("Empty table loads", () => {
-        let mockAdapter = new MockAdapter(axios);
-        mockAdapter.onGet('http://localhost:8000/hotline/api/servicerequests/').reply(200, {
-            service_requests: []
-        })
-        const {getByText } = render(<ServiceRequestTable />)
-        expect(getByText(/No Service Requests found./)).toBeTruthy()
-    });
-    it(" Table with 1 SR loads", () => {
-        let mockAdapter = new MockAdapter(axios);
-        mockAdapter.onGet('http://localhost:8000/hotline/api/servicerequests/').reply(200, {
-            service_requests:[{'id': 3}]
-        })
-        const {getByText } = render(<ServiceRequestTable />)
-        expect(getByText(/No Service Requests found./)).toBeTruthy()
-    });
-})
+  it("Empty table loads", async () => {
+    render(<ServiceRequestTable />);
+    expect(await screen.getByText("Fetching service requests...")).toBeTruthy();
+  });
+});
