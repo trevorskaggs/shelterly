2b2c62a0d89a3cf9823d013c83d04334789d69c8
Alex Mountain <alexander.g.mountain@gmail.com>
2020-09-28 21:54:36 -0400

    Add 1mile circle toggle around markers.

--- frontend/src/evac/EvacViews.js
+++ frontend/src/evac/EvacViews.js
@@ -4,9 +4,9 @@ import { Link } from 'raviger';
 import { Button, Col, Container, Form, FormCheck, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faBandAid, faCar, faClipboardList, faShieldAlt, faTrailer
+  faBandAid, faBullseye, faCar, faClipboardList, faShieldAlt, faTrailer
 } from '@fortawesome/free-solid-svg-icons';
-import { CircleMarker, Map, TileLayer, Tooltip as MapTooltip, useLeaflet } from "react-leaflet";
+import { Circle, CircleMarker, Map, TileLayer, Tooltip as MapTooltip, useLeaflet } from "react-leaflet";
 import L from "leaflet";
 import shield from "../static/images/shield-alt-solid.png";
 import bandaid from "../static/images/band-aid-solid.png";
@@ -33,17 +33,34 @@ export function Dispatch() {
   const [totalSelectedState, setTotalSelectedState] = useState({});
   const [selectedCount, setSelectedCount] = useState({count:0, disabled:true});
   const [statusOptions, setStatusOptions] = useState({aco_required:false});
+  const [radiusCircles, setRadiusCircles] = useState({circles:[]});
 
   // Handle aco_required toggle.
   const handleACO = async event => {
     setStatusOptions({aco_required:!statusOptions.aco_required})
   }
 
+  // Handle radius circle toggles.
+  const handleRadius = (latitude, longitude, id) => {
+    if (mapState[id].radius === "disabled") {
+      setMapState(prevState => ({ ...prevState, [id]: {color:mapState[id].color, checked:mapState[id].checked, hidden:mapState[id].hidden, matches:mapState[id].matches, radius:"enabled"} }));
+      setRadiusCircles(prevState => ({
+        circles: [...prevState.circles, {latitude: latitude, longitude:longitude}]
+      }));
+    }
+    else {
+      setMapState(prevState => ({ ...prevState, [id]: {color:mapState[id].color, checked:mapState[id].checked, hidden:mapState[id].hidden, matches:mapState[id].matches, radius:"disabled"} }));
+      setRadiusCircles(prevState => ({
+        circles: prevState.circles.filter(circle => (circle.latitude != latitude && circle.longitude !=longitude) )
+    }));
+    }
+  }
+
   // Handle dynamic SR state and map display when an SR is selected or deselected.
   const handleMapState = (id) => {
     // If selected.
     if (mapState[id].checked === false) {
-      setMapState(prevState => ({ ...prevState, [id]: {color:"green", checked:true, hidden:mapState[id].hidden, matches:mapState[id].matches} }));
+      setMapState(prevState => ({ ...prevState, [id]: {color:"green", checked:true, hidden:mapState[id].hidden, matches:mapState[id].matches, radius:mapState[id].radius} }));
       var matches = {};
       // Add each match count to the running total state tracker.
       for (var key in mapState[id].matches) {
@@ -61,7 +78,7 @@ export function Dispatch() {
     }
     // Else deselect.
     else {
-      setMapState(prevState => ({ ...prevState, [id]: {color:"red", checked:false, hidden:mapState[id].hidden, matches:mapState[id].matches} }));
+      setMapState(prevState => ({ ...prevState, [id]: {color:"red", checked:false, hidden:mapState[id].hidden, matches:mapState[id].matches, radius:mapState[id].radius} }));
       // Remove matches from the running total state tracker.
       for (var key in mapState[id].matches) {
         var total = totalSelectedState[key] -= mapState[id].matches[key];;
@@ -121,10 +138,10 @@ export function Dispatch() {
     for (const service_request of data.service_requests) {
       if (mapState[service_request.id]) {
         if (!event.target.getBounds().contains(L.latLng(service_request.latitude, service_request.longitude))) {
-          setMapState(prevState => ({ ...prevState, [service_request.id]: {color:mapState[service_request.id].color, checked:mapState[service_request.id].checked, hidden:true, matches:mapState[service_request.id].matches} }));
+          setMapState(prevState => ({ ...prevState, [service_request.id]: {color:mapState[service_request.id].color, checked:mapState[service_request.id].checked, hidden:true, matches:mapState[service_request.id].matches, radius:mapState[service_request.id].radius} }));
         }
         else {
-          setMapState(prevState => ({ ...prevState, [service_request.id]: {color:mapState[service_request.id].color, checked:mapState[service_request.id].checked, hidden:false, matches:mapState[service_request.id].matches} }));
+          setMapState(prevState => ({ ...prevState, [service_request.id]: {color:mapState[service_request.id].color, checked:mapState[service_request.id].checked, hidden:false, matches:mapState[service_request.id].matches, radius:mapState[service_request.id].radius} }));
         }
       }
     }
@@ -152,7 +169,7 @@ export function Dispatch() {
           const bounds = [];
           for (const service_request of response.data) {
             const matches = countMatches(service_request);
-            map_dict[service_request.id] = {color:"red", checked:false, hidden:false, matches:matches};
+            map_dict[service_request.id] = {color:"red", checked:false, hidden:false, matches:matches, radius:"disabled"};
             bounds.push([service_request.latitude, service_request.longitude]);
           }
           setMapState(map_dict);
@@ -191,9 +208,8 @@ export function Dispatch() {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-            {data.service_requests.map(service_request => (
+            {data.service_requests.map(service_request => (<span key={service_request.id}>
               <CircleMarker
-                key={service_request.id}
                 center={{lat:service_request.latitude, lng: service_request.longitude}}
                 color={mapState[service_request.id] ? mapState[service_request.id].color : ""}
                 fill={true}
@@ -219,6 +235,10 @@ export function Dispatch() {
                   </span>
                 </MapTooltip>
               </CircleMarker>
+              {radiusCircles.circles.map(circle => (
+                <Circle key={circle} center={{lat:circle.latitude, lng: circle.longitude}} radius={805} />
+              ))}
+              </span>
             ))}
           </Map>
         </Col>
@@ -272,7 +292,7 @@ export function Dispatch() {
                 </Tooltip>
               }
             >
-              <FontAwesomeIcon icon={faCar} inverse className="ml-1"/>
+              <FontAwesomeIcon icon={faCar} className="ml-1"/>
             </OverlayTrigger>
              : ""}
             {service_request.turn_around ?
@@ -285,10 +305,11 @@ export function Dispatch() {
                 </Tooltip>
               }
             >
-              <FontAwesomeIcon icon={faTrailer} inverse className="ml-1"/>
+              <FontAwesomeIcon icon={faTrailer} className="ml-1"/>
             </OverlayTrigger>
              : ""}
             <span className="ml-2">| &nbsp;{service_request.full_address}</span>
+            <FontAwesomeIcon icon={faBullseye} className="ml-1" onClick={() => handleRadius(service_request.latitude, service_request.longitude, service_request.id)} />
             <Link href={"/hotline/servicerequest/" + service_request.id} target="_blank"> <FontAwesomeIcon icon={faClipboardList} inverse /></Link>
           </div>
         </div>
