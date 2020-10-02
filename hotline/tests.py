d852d3f643f084b7989d19d1551fe7dfe6d55ad2
Alex Mountain <alexander.g.mountain@gmail.com>
2020-10-02 15:12:23 -0400

    ok... throws another dart at tests.

--- hotline/tests.py
+++ hotline/tests.py
@@ -53,7 +53,7 @@ class TestViews(APITestCase):
         self.new_animal = Animal.objects.create(name='Henry', owner=self.new_person)
         self.client.force_authenticate(self.user)
         # Directions are currently a required field.
-        response = self.client.post(f'/hotline/api/servicerequests/', {'owner':self.new_person.pk, 'address':"123 Main St.", 'directions':"Turn left.", 'latitude':self.new_person.latitude, 'longitude':self.new_person.longitude})
+        response = self.client.post(f'/hotline/api/servicerequests/', {'owner':self.new_person.pk, 'address':"123 Main St.", 'directions':"Turn left.", 'latitude':0, 'longitude':0})
         self.assertEqual(response.status_code, 201)
         self.assertTrue(ServiceRequest.objects.filter(owner=self.new_person.pk, address="123 Main St."))
 
