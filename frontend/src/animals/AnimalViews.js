71a5197b4a2878076d90a760de47883454a6d819
Alex Mountain <alexander.g.mountain@gmail.com>
2020-10-11 22:16:22 -0400

    Progress improving look and feel on animal details.

--- frontend/src/animals/AnimalViews.js
+++ frontend/src/animals/AnimalViews.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import { Link } from 'raviger';
 import Moment from 'react-moment';
 import { Carousel } from 'react-responsive-carousel';
+import { Card, Col, ListGroup, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faClipboardList, faEdit,
+  faBandAid, faClipboardList, faCut, faEdit, faShieldAlt, faWarehouse
 } from '@fortawesome/free-solid-svg-icons';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -76,56 +77,181 @@ export function AnimalView({id}) {
 
   return (
     <>
-      {/* <h1 style={header_style}>
-        Animal Details - {data.status}<Link href={"/animals/animal/edit/" + id}> <FontAwesomeIcon icon={faEdit} inverse /></Link>
-      </h1> */}
-      <br/><br/>
-      <div style={card_style} className="card card-body bg-light mb-2 mx-auto">
-        <div className="row">
-          <div className="col-6">
-            <p><b>Name:</b> {data.name||"Unknown"}</p>
-            <p><b>Species:</b> {data.species}</p>
-            <p><b>Age:</b> {data.age||"Unknown"}</p>
-            <p><b>Sex:</b> {data.sex||"Unknown"}</p>
-            <p><b>Size:</b> {data.size}</p>
-            {data.last_seen ? <p><b>Last Seen:</b> <Moment format="LLL">{data.last_seen}</Moment></p> : ""}
-            {data.pcolor ? <p><b>Primary Color:</b> {data.pcolor}</p> : ""}
-            {data.scolor ? <p><b>Secondary Color:</b> {data.scolor}</p> : ""}
-            {data.color_notes ? <p><b>Color Notes:</b> {data.color_notes}</p> : ""}
-            {data.behavior_notes ? <p><b>Behavior Notes:</b> {data.behavior_notes}</p> : ""}
-          </div>
-          <div className="col-6">
-          <div className="slide-container float-right" style={{width:"490px", height:"322px"}}>
-          <Carousel className="carousel-wrapper" showThumbs={false} showStatus={false}>
-            {images.map(image => (
-              <div key={image} className="image-container">
-                <img src={image} />
-              </div>
-            ))}
-            </Carousel>
-          </div>
-            {/* <p><b>Fixed:</b> {data.fixed}</p>
-            <p><b>Aggressive:</b> {data.aggressive}</p>
-            <p><b>Confined:</b> {data.confined}</p>
-            <p><b>Injured:</b> {data.injured}</p> */}
-          </div>
-        </div>
+    <div className="row mt-3" style={{marginBottom:"-8px"}}>
+      <div className="col-12 d-flex">
+        <h1 style={header_style}>
+          Animal Details - {data.status}<Link href={"/animals/animal/edit/" + id}> <FontAwesomeIcon icon={faEdit} inverse /></Link>
+        </h1>
       </div>
-      <div style={card_style} className="card card-body bg-light mb-2 mx-auto">
-        <div className="row">
-          <div className="col-10">
-            {data.request ? <div><b>Request #{data.request}</b><Link href={"/hotline/servicerequest/" + data.request}> <FontAwesomeIcon icon={faClipboardList} inverse /></Link></div> : ""}
-            {data.owner ? <div><b>Owner:</b> {data.owner_object.first_name} {data.owner_object.last_name} {data.owner_object.phone} {data.owner_object.first_name !== 'Unknown' ? <Link href={"/hotline/owner/" + data.owner}> <FontAwesomeIcon icon={faClipboardList} inverse /></Link> : <Link href={"/hotline/owner/edit/" + data.owner}> <FontAwesomeIcon icon={faEdit} inverse /></Link>}</div> : ""}
-            <div><b>Location:</b> {data.full_address ? data.full_address : "Unknown"}</div>
+    </div>
+    <hr/>
+    <div className="row mb-2">
+      <div className="col-6 d-flex" style={{marginRight:"-15px"}}>
+        <Card className="mb-2 border rounded" style={{width:"100%"}}>
+          <Card.Body>
+            <Card.Title>
+              Information
+              {data.aggressive ?
+                <OverlayTrigger
+                  key={"aggressive"}
+                  placement="top"
+                  overlay={
+                    <Tooltip id={`tooltip-aggressive`}>
+                      Animal is aggressive
+                    </Tooltip>
+                  }
+                >
+                  <FontAwesomeIcon icon={faShieldAlt} size="sm" className="ml-1" />
+                </OverlayTrigger> :
+              ""}
+              {data.fixed ?
+                <OverlayTrigger
+                  key={"fixed"}
+                  placement="top"
+                  overlay={
+                    <Tooltip id={`tooltip-fixed`}>
+                      Animal is fixed or neutered
+                    </Tooltip>
+                  }
+                >
+                  <FontAwesomeIcon icon={faCut} size="sm" className="ml-1" />
+                </OverlayTrigger> :
+              ""}
+              {data.injured ?
+                <OverlayTrigger
+                  key={"injured"}
+                  placement="top"
+                  overlay={
+                    <Tooltip id={`tooltip-injured`}>
+                      Animal is injured
+                    </Tooltip>
+                  }
+                >
+                  <FontAwesomeIcon icon={faBandAid} size="sm" className="ml-1" />
+                </OverlayTrigger> :
+              ""}
+              {data.confined ?
+                <OverlayTrigger
+                  key={"confined"}
+                  placement="top"
+                  overlay={
+                    <Tooltip id={`tooltip-confined`}>
+                      Animal is confined
+                    </Tooltip>
+                  }
+                >
+                  <FontAwesomeIcon icon={faWarehouse} size="sm" className="ml-1" />
+                </OverlayTrigger> :
+              ""}
+            </Card.Title>
+              <hr/>
+              <ListGroup variant="flush">
+                <ListGroup.Item><b>Name:</b> {data.name||"Unknown"}</ListGroup.Item>
+                <ListGroup.Item><b>Species:</b> {data.species}</ListGroup.Item>
+                <ListGroup.Item><b>Age:</b> {data.age||"Unknown"}</ListGroup.Item>
+                <ListGroup.Item><b>Sex:</b> {data.sex||"Unknown"}</ListGroup.Item>
+                <ListGroup.Item><b>Size:</b> {data.size}</ListGroup.Item>
+                {data.last_seen ? <ListGroup.Item><b>Last Seen:</b> <Moment format="LLL">{data.last_seen}</Moment></ListGroup.Item> : ""}
+                {data.pcolor ? <ListGroup.Item><b>Primary Color:</b> {data.pcolor}</ListGroup.Item> : ""}
+                {data.scolor ? <ListGroup.Item><b>Secondary Color:</b> {data.scolor}</ListGroup.Item> : ""}
+                {data.color_notes ? <ListGroup.Item><b>Color Notes:</b> {data.color_notes}</ListGroup.Item> : ""}
+                {data.behavior_notes ? <ListGroup.Item><b>Behavior Notes:</b> {data.behavior_notes}</ListGroup.Item> : ""}
+              </ListGroup>
+            </Card.Body>
+          </Card>
           </div>
+          <Col xs={6} className="d-flex mb-2 pr-0">
+            <div className="slide-container flex-grow-1 border rounded pl-0 pr-0" style={{width:"490px", height:"322px"}}>
+              <Carousel className="carousel-wrapper" showThumbs={false} showStatus={false}>
+                {images.map(image => (
+                  <div key={image} className="image-container">
+                    <img src={image} />
+                  </div>
+                ))}
+              </Carousel>
+            </div>
+          </Col>
         </div>
-      </div>
-      <hr/>
-      <div style={btn_style}>
-        <Link href={"/animals/animal/edit/" + id} style={link_style} className="btn btn-primary btn-lg btn-block mb-2">EDIT ANIMAL</Link>
-        <br/>
-        <br/>
-        <Link className="btn btn-secondary btn-lg btn-block" href="/hotline/">BACK</Link>
+      <div className="row mb-2">
+        <div className="col-6 d-flex">
+          <Card className="mb-2 border rounded" style={{width:"100%"}}>
+            <Card.Body>
+              <Card.Title>
+                <h4 className="mb-0">Location
+                  {/* {data.verbal_permission ?
+                  <OverlayTrigger
+                    key={"verbal"}
+                    placement="top"
+                    overlay={
+                      <Tooltip id={`tooltip-verbal`}>
+                        Verbal permission granted
+                      </Tooltip>
+                    }
+                  >
+                    <FontAwesomeIcon icon={faComment} size="sm" className="ml-1" />
+                  </OverlayTrigger> : ""}
+                  {data.key_provided ?
+                  <OverlayTrigger
+                    key={"key"}
+                    placement="top"
+                    overlay={
+                      <Tooltip id={`tooltip-key`}>
+                        Key provided
+                      </Tooltip>
+                    }
+                  >
+                    <FontAwesomeIcon icon={faKey} size="sm" className="ml-1" />
+                  </OverlayTrigger> : ""}
+                  {data.accessible ?
+                  <OverlayTrigger
+                    key={"accessible"}
+                    placement="top"
+                    overlay={
+                      <Tooltip id={`tooltip-accessible`}>
+                        Easily accessible
+                      </Tooltip>
+                    }
+                  >
+                    <FontAwesomeIcon icon={faCar} size="sm" className="ml-1" />
+                  </OverlayTrigger> : ""}
+                  {data.turn_around ?
+                  <OverlayTrigger
+                    key={"turnaround"}
+                    placement="top"
+                    overlay={
+                      <Tooltip id={`tooltip-turnaround`}>
+                        Room to turn around
+                      </Tooltip>
+                    }
+                  >
+                    <FontAwesomeIcon icon={faTrailer} size="sm" className="ml-1" />
+                  </OverlayTrigger> : ""} */}
+                </h4>
+              </Card.Title>
+              <hr/>
+              <ListGroup variant="flush">
+                {data.owner ? <ListGroup.Item style={{marginTop:"-13px"}}><b>Address:</b> {data.owner_object.address ? <span>{data.owner_object.full_address}</span> : 'N/A'}</ListGroup.Item> :""}
+                {/* <ListGroup.Item style={{marginBottom:"-13px"}}><b>Directions:</b> {data.directions}</ListGroup.Item> */}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-6 d-flex pl-0">
+          <Card className="mb-2 border rounded" style={{width:"100%"}}>
+            <Card.Body>
+              {data.owner ?
+              <span>
+                <Card.Title>
+                  <h4 className="mb-0">Owner: <span style={{fontSize:18}}>{data.owner_object.first_name} {data.owner_object.last_name} {data.owner_object.first_name !== 'Unknown' ? <Link href={"/hotline/owner/" + data.owner}> <FontAwesomeIcon icon={faClipboardList} size="sm" inverse /></Link>:""}<Link href={"/hotline/owner/edit/" + data.owner}> <FontAwesomeIcon icon={faEdit} size="sm" inverse /></Link></span></h4>
+                </Card.Title>
+                <ListGroup variant="flush" style={{marginTop:"-13px", marginBottom:"-13px"}}>
+                  {data.owner_object.phone ? <ListGroup.Item><b>Telephone: </b>{data.owner_object.phone}</ListGroup.Item> : ""}
+                  {data.owner_object.email ? <ListGroup.Item><b>Email: </b>{data.owner_object.email}</ListGroup.Item> : ""}
+                </ListGroup>
+              </span> : ""}
+            </Card.Body>
+          </Card>
+        </div>
       </div>
     </>
   );
