9a0504871cfdc350bfaa1491eb13380ae161b0ca
trevorskaggs <skaggs.trevor@gmail.com>
2020-10-11 18:32:00 -0700

    Evac assignments (#81)
    * Migrations, initial view/urls/api endpoint
    
    * Add New EvacTeamMember Form
    
    * Build autocomplete component
    
    * Use typeahead for team selection
    
    * BRING BACK ADD TEAM MEMBER
    
    * User serializer field even
    
    * Build basic form, generate EA endpoint. wait for SR hook!
    
    * Clean up unused vars, update selection url
    
    * Fix tests
    
    * Annnnnd again
    
    * Remove junk
    
    * Minor model and form tweaks.
    
    * Remove migrations
    
    * MVP MVP!
    
    * Fix test
    
    * But also the other one
    
    * Leave this for the docker branch
    
    * Add djangoadmin
    
    * Add font color override for typeahead
    
    * make 2 buttons, hook in team members
    
    * Fix evac url on submit.
    
    * Revert
    
    Co-authored-by: Trevor Skaggs <tskaggs.ctr@element84.com>
    Co-authored-by: Alex Mountain <alexander.g.mountain@gmail.com>

--- evac/forms.py
+++ evac/forms.py
@@ -1,9 +1,2 @@
 from django import forms
-from evac.models import EvacTeam
-
-class EvacTeamForm(forms.ModelForm):
-
-    class Meta:
-        model = EvacTeam
-        fields = ['evac_team_members', 'callsign']
 
