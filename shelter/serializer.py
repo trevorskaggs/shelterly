4a860b55fd5a67727fa8ca59d89d48e81652102f
Alex Mountain <alexander.g.mountain@gmail.com>
2020-10-20 13:59:07 -0400

    Add shelter room picker to Animal form.

--- shelter/serializer.py
+++ shelter/serializer.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from .models import *
 from location.utils import build_full_address
-
+from animals.models import Animal
+from animals.serializers import AnimalSerializer
+from shelter.models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
+    animals = AnimalSerializer(source='animal_set', many=True, required=False, read_only=True)
+
     class Meta:
         model = Room
         fields = (
@@ -11,16 +15,21 @@ class RoomSerializer(serializers.ModelSerializer):
             'building',
             'name',
             'description',
+            'animals'
         )
 
 class BuildingSerializer(serializers.ModelSerializer):
     shelter_name = serializers.SerializerMethodField()
+    animal_count = serializers.SerializerMethodField()
     rooms = RoomSerializer(source='room_set', many=True, required=False, read_only=True)
 
     # Custom field for the shelter name.
     def get_shelter_name(self, obj):
         return obj.shelter.name
 
+    # Custom field for total animals.
+    def get_animal_count(self, obj):
+        return Animal.objects.filter(room__in=obj.room_set.all()).count()
 
     class Meta:
         model = Building
@@ -29,17 +38,28 @@ class BuildingSerializer(serializers.ModelSerializer):
             'name',
             'shelter',
             'shelter_name',
+            'animal_count',
             'description',
             'rooms'
         )
 
 class ShelterSerializer(serializers.ModelSerializer):
     full_address = serializers.SerializerMethodField()
+    animal_count = serializers.SerializerMethodField()
+    room_count = serializers.SerializerMethodField()
     buildings = BuildingSerializer(source='building_set', many=True, required=False, read_only=True)
 
     # Custom field for the full address.
     def get_full_address(self, obj):
         return build_full_address(obj)
+
+    # Custom field for total animals.
+    def get_animal_count(self, obj):
+        return Animal.objects.filter(room__building__in=obj.building_set.all()).count()
+
+    # Custom field for total rooms.
+    def get_room_count(self, obj):
+        return Room.objects.filter(building__in=obj.building_set.all()).count()
 
     # Truncates latitude and longitude.
     def to_internal_value(self, data):
