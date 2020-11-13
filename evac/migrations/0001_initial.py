196b75c3367ed582fdf7a3aaafbe2ae203adc1cb
Trevor Skaggs <skaggs.trevor@gmail.com>
2020-11-13 02:09:39 +0000

    Remove issue from migration

--- evac/migrations/0001_initial.py
+++ evac/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='EvacTeam',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('evac_team_members', models.ManyToManyField(to='people.TeamMember')),
+                #('evac_team_members', models.ManyToManyField(to='people.TeamMember')),
             ],
         ),
     ]
