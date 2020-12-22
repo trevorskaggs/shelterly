b58b5e3390bc7292e9ae923a209533e68fc615e1
Alex Mountain <alexander.g.mountain@gmail.com>
2020-12-21 19:23:35 -0500

    Migration shuffle.

--- people/migrations/0010_person_alt_phone.py
+++ people/migrations/0010_person_alt_phone.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('people', '0008_auto_20201104_1730'),
+        ('people', '0009_personchange'),
     ]
 
     operations = [
