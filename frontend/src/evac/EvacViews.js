4156ba68d8aa6457445b29c96801b674845d7283
Alex Mountain <alexander.g.mountain@gmail.com>
2021-01-05 15:33:06 -0500

    Lowercase i.

--- frontend/src/evac/EvacViews.js
+++ frontend/src/evac/EvacViews.js
@@ -577,7 +577,7 @@ export function EvacSummary({id}) {
           <ListGroup variant="flush" style={{marginTop:"-13px", marginBottom:"-13px"}}>
             <h4 className="mt-2" style={{marginBottom:"-2px"}}>Previous Note - <Moment format="L">{service_request.visit_notes.sort((a,b) => new Date(b.date_completed).getTime() - new Date(a.date_completed).getTime())[0].date_completed}</Moment></h4>
               <ListGroup.Item>
-              {service_request.visit_notes.sort((a,b) => new Date(b.date_completed).getTime() - new Date(a.date_completed).getTime())[0].notes || "No Information available."}
+              {service_request.visit_notes.sort((a,b) => new Date(b.date_completed).getTime() - new Date(a.date_completed).getTime())[0].notes || "No information available."}
               </ListGroup.Item>
           </ListGroup>
         : "" }
@@ -586,7 +586,7 @@ export function EvacSummary({id}) {
             <h4 className="mt-2" style={{marginBottom:"-2px"}}>Notes</h4>
             {service_request.visit_notes.filter(note => String(note.evac_assignment) === String(id)).map((note) => (
               <ListGroup.Item key={note.id}>
-                {note.notes || "No Information available."}
+                {note.notes || "No information available."}
               </ListGroup.Item>
             ))}
           </ListGroup>
