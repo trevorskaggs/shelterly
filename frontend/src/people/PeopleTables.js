625df6a41c3e792162513db955a8a684403c8753
Chris Vieu <cdvieu@gmail.com>
2021-01-07 16:58:23 -0500

    Displaying the Agency name for First Responders only

--- frontend/src/people/PeopleTables.js
+++ frontend/src/people/PeopleTables.js
@@ -30,6 +30,7 @@ export function PeopleTable() {
         })
         .then(response => {
             setData({owners: response.data, isFetching: false});
+            console.log(response.data);
         })
         .catch(error => {
             console.log(error.response);
@@ -84,11 +85,12 @@ export function PeopleTable() {
                 <div key={owner.id} className="mt-3">
                     <div className="mt-3">
                         <div className="card-header"> {owner.first_name ?
-                            <h4 style={{marginBottom:"-2px"}}>{owner.first_name} {owner.last_name}
-                                {owner.is_owner  ?
+                          <h4 style={{marginBottom: "-2px"}}>{owner.first_name} {owner.last_name}
+                              {owner.agency ? <span> ({owner.agency})</span> : <span></span>}
+                              {owner.is_owner ?
                                     <Link href={"/hotline/owner/" + owner.id} target="_blank"> <FontAwesomeIcon icon={faClipboardList} inverse/></Link>
                                     : <Link href={"/hotline/reporter/" + owner.id} target="_blank"> <FontAwesomeIcon icon={faClipboardList} inverse/></Link>}
-                    </h4> : "Unknown"}
+                          </h4> : "Unknown"}
                         </div>
                         <CardGroup>
                             <Card>
