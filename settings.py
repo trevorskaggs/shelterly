e7241f11dc5c024de87f2c2b21ae3be23c049c19
Trevor Skaggs <skaggs.trevor@gmail.com>
2021-01-15 16:42:27 +0000

    Fix static file issue

--- settings.py
+++ settings.py
@@ -22,9 +22,9 @@ except FileNotFoundError:
     print("No secrets file found. Environmental variables must be set elsewhere.")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'shelterly/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -132,10 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "shelterly/frontend/src/static/")
+    os.path.join(BASE_DIR, "frontend/src/static/")
 ]
 # Dev settings. Remove when deploying to Zappa
-STATIC_ROOT=os.path.join(BASE_DIR, 'shelterly/static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 # SECURE_CONTENT_TYPE_NOSNIFF = False
 # DEBUG = True
 
