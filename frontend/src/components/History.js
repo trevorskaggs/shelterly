fac3fb5cb052b8cf7d9195701d6e3f60b2706845
spencercarroll <scarroll10@csuchico.edu>
2021-03-01 18:38:47 -0500

    add index

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -10,29 +10,31 @@ function History({action_history}) {
 
   const [showHistory, setShowHistory] = useState(false);
   const [page, setPage] = useState(1)
-  const numPages = Math.ceil(action_history.length / ITEMS_PER_PAGE)
+  const [numPages, setNumPages] = useState(1)
 
   return (
     <>
     <hr/>
     <h1 className="mb-3">History<FontAwesomeIcon icon={faChevronCircleRight} hidden={showHistory} onClick={() => setShowHistory(!showHistory)} className="ml-2" style={{verticalAlign:"middle"}} inverse /><FontAwesomeIcon icon={faChevronCircleDown} hidden={!showHistory} onClick={() => setShowHistory(!showHistory)} className="ml-2" style={{verticalAlign:"middle"}} inverse /></h1>
-    {action_history.map(action => (
-    <Collapse key={action + Math.random()} in={showHistory}>
-      <div>
-        <Card className="border rounded d-flex mb-2" style={{width:"100%"}}>
-          <Card.Body>
-            {action}
-          </Card.Body>
-        </Card>
-      </div>
-      <Pagination className="custom-page-links" size="lg" onClick={(e) => {setPage(parseInt(e.target.innerText))}}>
-        {[...Array(numPages).keys()].map(x => 
-        <Pagination.Item key={x+1} active={x+1 === page}>
-                  {x+1}
-                </Pagination.Item>)
-        }
-      </Pagination>
-    </Collapse>
+    {action_history.map((action, index) => (
+    <div key={service_request.id} className="mt-3" hidden={page !== Math.ceil((index+1)/ITEMS_PER_PAGE)}>
+      <Collapse key={action + Math.random()} in={showHistory}>
+        <div>
+          <Card className="border rounded d-flex mb-2" style={{width:"100%"}}>
+            <Card.Body>
+              {action}
+            </Card.Body>
+          </Card>
+        </div>
+        <Pagination className="custom-page-links" size="lg" onClick={(e) => {setPage(parseInt(e.target.innerText))}}>
+          {[...Array(numPages).keys()].map(x => 
+          <Pagination.Item key={x+1} active={x+1 === page}>
+                    {x+1}
+                  </Pagination.Item>)
+          }
+        </Pagination>
+      </Collapse>
+    </div>
     ))}
     </>
   );
