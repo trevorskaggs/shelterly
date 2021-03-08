cc088ded3a9bc437203d48c93fc619d5bc07c675
Alex Mountain <alexander.g.mountain@gmail.com>
2021-03-08 13:59:45 -0500

    Fix dupe Person bug when hitting dupe SR location error.

--- frontend/src/hotline/ServiceRequestForm.js
+++ frontend/src/hotline/ServiceRequestForm.js
@@ -132,9 +132,9 @@ function ServiceRequestForm(props) {
             .then(response => {
               props.state.steps.animals.forEach(animal => {
                 // Add owner and reporter to animal data.
-                animal['reporter'] = reporterResponse[0].data.id
-                animal['new_owner'] = ownerResponse[0].data.id
-                animal['request'] = response.data.id
+                animal.append('reporter', reporterResponse[0].data.id);
+                animal.append('new_owner', ownerResponse[0].data.id);
+                animal.append('request', response.data.id);
                 axios.post('/animals/api/animal/', animal)
                 .catch(error => {
                   console.log(error.response);
@@ -280,7 +280,7 @@ function ServiceRequestForm(props) {
         <Modal.Body>
           <p>
             {error && error.error[0]}
-            &nbsp;Click <Link href={'/hotline/servicerequest/' + error.error[1]} target="_blank">here</Link> to view this Request.
+            &nbsp;Click <Link href={'/hotline/servicerequest/' + error.error[1]} target="_blank" style={{color:"#8d99d4"}}>here</Link> to view this Request.
           </p>
         </Modal.Body>
         <Modal.Footer>
