8e8e64adc2d3bdba3798b04a92692a6008fc540c
Robbie Meyers <robbie.meyers@gmail.com>
2021-03-29 19:04:46 -0400

    order of ops

--- scripts/import-resources.py
+++ scripts/import-resources.py
@@ -1,13 +1,12 @@
 import sys
 sys.path.append('/home/sheltuser/shelterly/')
 import django
+django.setup()
 from django.conf import settings
 from accounts.models import ShelterlyUser
 from evac.models import EvacTeamMember
 from django.db import IntegrityError
 import csv
-
-django.setup()
 
 with open(sys.argv[1], newline='', encoding='utf-8-sig') as f:
     reader = csv.DictReader(f, fieldnames=['first_name','last_name','email','cell_phone','agency_id','shelterly_user', 'team_member'])
