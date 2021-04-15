eec5d240479a923f9ba37dfe8dbfb268798ce7ad
Robbie Meyers <robbie.meyers@gmail.com>
2021-04-15 15:02:06 -0400

    use urls for src instead of import

--- frontend/src/hotline/Hotline.js
+++ frontend/src/hotline/Hotline.js
@@ -124,10 +124,10 @@ function Hotline() {
                       {service_request.full_address}
                       {service_request.followup_date ? <div>Followup Date: <Moment format="L">{service_request.followup_date}</Moment></div> : ""}
                       <div>
-                        {service_request.aco_required ? <img width={16} height={16} src={badge} alt="" className="mr-1" /> : ""}
-                        {service_request.injured ? <img width={16} height={16} src={bandaid} alt="" className="mr-1" /> : ""}
-                        {service_request.accessible ? <img width={16} height={16} src={car} alt="" className="mr-1" /> : ""}
-                        {service_request.turn_around ? <img width={16} height={16} src={trailer} alt="" /> : ""}
+                      {service_request.aco_required ? <img width={16} height={16} src={`${S3_BUCKET}images/badge-sheriff.png`} alt="" className="mr-1" /> : ""}
+                        {service_request.injured ? <img width={16} height={16} src={`${S3_BUCKET}images/band-aid-solid.png`} alt="" className="mr-1" /> : ""}
+                        {service_request.accessible ? <img width={16} height={16} src={`${S3_BUCKET}images/car-solid.png`} alt="" className="mr-1" /> : ""}
+                        {service_request.turn_around ? <img width={16} height={16} src={`${S3_BUCKET}images/trailer-solid.png`} alt="" /> : ""}
                       </div>
                     </span>
                   </MapTooltip>
