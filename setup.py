5a54cf0f4f231af56deb163cc76727520a4a2915
Trevor Skags <skaggs.trevor@gmail.com>
2021-08-21 05:58:25 -0700

    Remove packages

--- setup.py
+++ setup.py
@@ -7,5 +7,4 @@ setup(name="shelterly",
       author="Trevor Skaggs",
       authoer_email="skaggs.trevor@gmail.com",
       url='https://github.com/trevorskaggs/shelterly',
-      packages=['accounts', 'animals', 'evac', 'hotline', 'intake', 'location', 'people', 'shelter']
 )
