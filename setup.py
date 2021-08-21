31f5044d093ffbb0eec0d7d33ce2af62cdae6c6a
Trevor Skags <skaggs.trevor@gmail.com>
2021-08-21 05:50:52 -0700

    Add setup.py to support pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="shelterly",
+      version="1.0",
+      description="Shelterly Repo",
+      license='MIT',
+      author="Trevor Skaggs",
+      authoer_email="skaggs.trevor@gmail.com",
+      url='https://github.com/trevorskaggs/shelterly',
+      packages=['accounts', 'animals', 'evac', 'hotline', 'intake', 'location', 'people', 'shelter']
+)
