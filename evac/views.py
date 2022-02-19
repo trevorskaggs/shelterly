c7d6352b1f1aba9ec446115259e48a37f8d9d320
Alex Mountain <alexander.g.mountain@gmail.com>
2022-02-19 08:26:07 -0500

    Fix up owner contact.

--- evac/views.py
+++ evac/views.py
@@ -178,7 +178,7 @@ class EvacAssignmentViewSet(viewsets.ModelViewSet):
                         VisitNote.objects.filter(assigned_request=assigned_request).update(date_completed=service_request['date_completed'], notes=service_request['notes'], forced_entry=service_request['forced_entry'])
                 # Only create OwnerContact on first update, otherwise update existing OwnerContact.
                 if service_request.get('owner_contact_note'):
-                    owner = service_request['owner_contact_id'] if service_request.get('owner_contact_id') else None
+                    owner = Person.objects.get(pk=service_request['owner_contact_id']) if service_request.get('owner_contact_id') else None
                     owner_contact_time = service_request['owner_contact_time'] if service_request.get('owner_contact_time') else None
                     if not assigned_request.owner_contact:
                         owner_contact = OwnerContact.objects.create(owner=owner, owner_contact_note=service_request['owner_contact_note'], owner_contact_time=owner_contact_time)
