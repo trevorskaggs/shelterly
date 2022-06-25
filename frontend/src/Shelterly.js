942c7d2a8be141b3261e45ad047d8c1ddb93c309
Michael Parks <mparks@element84.com>
2022-06-25 15:59:45 -0400

    Adds lat/lon to dispatch assignment page and print (#434)
    * Adds lat/lon to dispatch assignment page and print
    (#415)
    
    * Move lat/lon, add logout changes
    
    Co-authored-by: Trevor Skaggs <skaggs.trevor@gmail.com>

--- frontend/src/Shelterly.js
+++ frontend/src/Shelterly.js
@@ -30,7 +30,7 @@ function Shelterly() {
           : ""}
           <span className='d-flex flex-column col-9 h-100' style={style}>
             <Fragment>
-              {routeResult || <PageNotFound cookies={cookies} />}
+              {routeResult || <PageNotFound state={state} />}
             </Fragment>
           </span>
         </Row>
