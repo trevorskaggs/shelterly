6049e9791b476e09e67363dd16c65d297333ecd0
Alex Mountain <alexander.g.mountain@gmail.com>
2022-07-08 11:59:37 -0400

    Update Django to 3.2.14.

--- settings.py
+++ settings.py
@@ -39,6 +39,8 @@ USE_S3 = True
 ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'accounts.ShelterlyUser'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 #Bump allowed filesize to 10MB
 
 # AWS Config
