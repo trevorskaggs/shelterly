22292b4da5e222b2a5b6e1d6b7a7b139b197c16f
Alex Mountain <alexander.g.mountain@gmail.com>
2023-02-06 15:05:59 -0500

    Accurate file name.

--- frontend/src/shelter/Utils.js
+++ frontend/src/shelter/Utils.js
@@ -11,6 +11,6 @@ export const printRoomAnimalCareSchedules  = async (animals = [], roomId = 0) =>
 
 export const printIntakeSummaryAnimalCareSchedules  = async (animals = [], roomId = 0) => {
   const pdf = await buildAnimalCareScheduleDoc(animals);
-  pdf.fileName = `Shelterly-Room-Animal-Care-Schedules-${roomId.toString().padStart(4, 0)}-${moment().format(dateFormat)}`;
+  pdf.fileName = `Shelterly-Intake-Animal-Care-Schedules-${roomId.toString().padStart(4, 0)}-${moment().format(dateFormat)}`;
   pdf.saveFile();
 };
