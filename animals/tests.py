45b45d60cc59f01759fa5fab3194055d90c5e8cc
Alex Mountain <alexander.g.mountain@gmail.com>
2025-04-13 10:38:53 -0400

    Update test.

--- animals/tests.py
+++ animals/tests.py
@@ -2,14 +2,15 @@ from rest_framework.test import APITestCase
 
 from accounts.models import ShelterlyUser
 from animals.models import Animal
-from incident.models import Incident
+from incident.models import Incident, Organization
 from people.models import Person
 
 class TestViews(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.incident = Incident.objects.create(name='Test1', slug='test1', latitude=0, longitude=0)
+        cls.organization = Organization.objects.create(name='Test1', slug='test1')
+        cls.incident = Incident.objects.create(name='Test1', slug='test1', latitude=0, longitude=0, organization=cls.organization)
         cls.user = ShelterlyUser.objects.create_user(email="test@test.com", cell_phone="5555555", password="test", is_active=True)
         cls.owner = Person.objects.create(first_name="Leroy", last_name="Jenkins", incident=cls.incident)
         cls.animal = Animal.objects.create(name='bella', incident=cls.incident)
@@ -50,6 +51,6 @@ class TestViews(APITestCase):
     def test_create_animals(self):
         self.new_owner = Person.objects.create(first_name="New", last_name="Person", incident=self.incident)
         self.client.force_authenticate(self.user)
-        response = self.client.post('/animals/api/animal/', {'new_owner':[self.new_owner.pk], 'name':'Phineas', 'incident_slug':self.incident.slug})
+        response = self.client.post('/animals/api/animal/', {'new_owner':[self.new_owner.pk], 'name':'Phineas', 'incident_slug':self.incident.slug, 'organization_slug':self.organization.slug})
         self.assertEqual(response.status_code, 201)
         self.assertTrue(Animal.objects.filter(owners=self.new_owner, name='Phineas', status="REPORTED").exists())
