bee00216a14ba49ac2dff67c980e143a00cbe3f8
Alex Mountain <alexander.g.mountain@gmail.com>
2025-04-16 12:14:53 -0400

    Migration to change followup_date on AssignedRequest.

--- evac/models.py
+++ evac/models.py
@@ -79,7 +79,7 @@ class AssignedRequest(models.Model):
     service_request = models.ForeignKey(ServiceRequest, null=True, on_delete=models.SET_NULL)
     dispatch_assignment = models.ForeignKey(EvacAssignment, null=True, on_delete=models.SET_NULL, related_name='assigned_requests')
     animals = models.JSONField()
-    followup_date = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    followup_date = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
     owner_contact = models.ForeignKey('people.OwnerContact', null=True, on_delete=models.CASCADE, related_name='assigned_request')
     visit_note = models.ForeignKey('hotline.VisitNote', null=True, on_delete=models.CASCADE, related_name='assigned_request')
     timestamp = models.DateTimeField(null=True, blank=True)
